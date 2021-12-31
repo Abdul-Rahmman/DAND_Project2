8f8eb516a3a831ccfd674b76920bf8e715ed9cae
Abdul-Rahmman <kudabaksh44@gmail.com>
2021-12-31 16:50:26 +0300

    Add files via upload

--- Data/process_data.py
+++ Data/process_data.py
@@ -5,59 +5,84 @@
 """
 
 # import libraries
-
+import sys
 import pandas as pd
 import numpy as np
 import sqlite3
 from sqlalchemy import create_engine
 
+def transform(messages_filepath, categories_filepath):
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
 
-# load messages dataset
-messages = pd.read_csv('./Data/messages.csv')
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
 
-# load categories dataset
-categories = pd.read_csv('./Data/categories.csv')
+    # merge datasets
+    df = pd.merge(messages, categories,  how='outer', on='id')
 
-# merge datasets
-df = pd.merge(messages, categories,  how='outer', on='id')
-
-# create a dataframe of the 36 individual category columns
-categories = df.categories.str.split(';',expand=True)
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';',expand=True)
 
 
-# select the first row of the categories dataframe
-row = categories.head(1)
+    # select the first row of the categories dataframe
+    row = categories.head(1)
 
-# use this row to extract a list of new column names for categories.
-# one way is to apply a lambda function that takes everything 
-# up to the second to last character of each string with slicing
-category_colnames = row.applymap(lambda x: x[:-2]).iloc[0,:]
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0,:]
 
-# rename the columns of `categories`
-categories.columns = category_colnames
+    # rename the columns of `categories`
+    categories.columns = category_colnames
 
-for column in categories:
-    # set each value to be the last character of the string
-    categories[column] = categories[column].astype(str).str[-1]
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
     
-    # convert column from string to numeric
-    categories[column] = categories[column].astype(int)
+       # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
     
-# drop the original categories column from `df`
-del df['categories']
+       # drop the original categories column from `df`
+    del df['categories']
 
-# concatenate the original dataframe with the new `categories` dataframe
-df = pd.concat([df,categories], axis = 1, join = 'inner' )
+       # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories], axis = 1, join = 'inner' )
 
-# drop duplicates
-df.drop_duplicates(inplace=True)
+    return df
 
-engine = create_engine('sqlite:///DataBase.db')
-df.to_sql('DataBase', engine, index=False)
+def clean(df):   
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    return df
 
-
-
-
-
+def save_data(df,db_name):
+    engine = create_engine('sqlite:///'+db_name')
+    df.to_sql('Disater_Response', engine, index=False)
     
     
+    
+    
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please make sure the provided filepath is in correct order.')
+
+
+if __name__ == '__main__':
+    main()
